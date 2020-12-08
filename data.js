85d07cd41c008269149a00916e1fc40008766dea
Natalia <nataliakit89@gmail.com>
2020-12-08 12:26:30 +0300

    v1

---
+++ data.js
@@ -0,0 +1,22 @@
+let COLUMNS = [
+  {id: 1, title: 'todo'},
+  {id: 2, title: 'dev'},
+  {id: 3, title: 'qa'},
+  {id: 4, title: 'done'},
+];
+
+let CARDS = [
+  {id: 1, title: 'first1', description: 'qweqw ef ewvgfwe few', reportId: 1, assignId: 2, columnId: 2},
+  {id: 2, title: 'first2', description: 'qweqw ef ewvgfwe few', reportId: 1, assignId: 2, columnId: 2},
+  {id: 3, title: 'first3', description: 'qweqw ef ewvgfwe few', reportId: 2, assignId: 1, columnId: 4},
+  {id: 4, title: 'first4', description: 'qweqw ef ewvgfwe few', reportId: 3, assignId: 2, columnId: 2},
+];
+
+let USERS = [
+  {id: 1, name: 'vova', img: '...'},
+  {id: 2, name: 'gena', img: '...'},
+  {id: 3, name: 'kote', img: '...'},
+  {id: 4, name: 'pes', img: '...'},
+];
+
+
---
+++ script.js
@@ -0,0 +1,102 @@
+
+const APP_ELEMENT = document.querySelector('.app');
+
+function getColumnHtml(column) {
+  return `<div class="column">
+    <div class="column__title">${column.title}</div>     
+    <div class="column__cards" data-id="${column.id}" ondrop="drop(event, this)" ondragover="allowDrow(event )"></div>     
+  </div>`;
+}
+
+function getCardHtml(card, columns) {
+  debugger
+  return `<div class="card" draggable="true" ondragstart="drag(event)">
+    <div class="card__top">
+      <div class="card__title">${card.title}</div>
+      <div class="card__id">${card.id}</div>
+    </div>
+    <div class="card__description">${card.description}</div>
+    <div class="card__bottom">
+      <div class="card__left">
+        <div class="card__column-list">${getColumnDropDown(columns, card.columnId)}</div>
+      <div class="card__right">
+        <div class="card__reports">
+          <div class="card__reporter">${card.reportId}</div>
+          <div class="card__reporter-img">${card.reportId}</div>
+        </div>
+        <div class="card__assigns">
+          <div class="card__assign">${card.assignId}</div>
+          <div class="card__assign-img">${card.assignId}</div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+}
+
+function getColumnDropDown(columns, currentColumnId) {
+  let optionHtml = columns.map((column) => {
+      let isSelect = currentColumnId === column.id;
+      let selectAttr = isSelect ? 'selected' : '';
+    return `<option value="${column.id}" ${selectAttr}>${column.title}</option>`;
+  });
+  return `<select class="card__select">${optionHtml}</select>`
+}
+
+function getAllColumns() {
+  return COLUMNS.map(column => getColumnHtml(column)).join('');
+}
+
+function getCardsHtml(cards) {
+  return cards.map(card => getCardHtml(card)).join('');
+}
+
+function getCardsByColumnId(columnId) {
+  let cards = CARDS.filter(card => card.columnId === columnId);
+  return getCardsHtml(cards);
+}
+
+function setCardsToColumns() {
+  COLUMNS.forEach(column => {
+    let cardsHtml = getCardsByColumnId(column.id);
+    let cardListElement = document.querySelector(`.column__cards[data-id="${column.id}"]`);
+    cardListElement.innerHTML = cardsHtml;
+  });
+}
+
+function main() {
+  APP_ELEMENT.innerHTML = getAllColumns();
+  setCardsToColumns();
+}
+
+main();
+
+let arr =[];
+function allowDrow(e) {
+  e.preventDefault();
+}
+function drag(e) {
+  e.dataTransfer.setData('text',e.target.id);
+  e.dataTransfer.setData('content',e.target.textContent);
+
+}
+function drop(e, block) {
+  debugger
+  e.preventDefault();
+
+  let data = e.dataTransfer.getData('text');
+  let content = e.dataTransfer.getData('content');
+
+  if(block.id === '1') {
+    if(arr.indexOf('content') == -1) {
+      arr.push(content)
+    }
+  }
+  if(block.id === '2') {
+    if(arr.indexOf('content') != -1) {
+      arr.splice(arr.indexOf(content), 1)
+    }
+  }
+  block.appendChild(document.getElementById(data));
+
+  console.log(drop);
+}
